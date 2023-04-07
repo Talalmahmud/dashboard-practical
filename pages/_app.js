5dba70658f53eb30e1e43bef87932262b959551b
Talal Mahmood <talalmahmud2011@gmail.com>
2023-04-07 14:37:20 +0600

    project initialize

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import Header from "@/components/Header";
+import CartProvider from "@/context/CartProvider";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <CartProvider>
+        <Header />
+        <Component {...pageProps} />
+      </CartProvider>
+    </>
+  );
 }
